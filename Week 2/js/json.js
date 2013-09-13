b87653661bdcf97891b7a739f18142c8054b8995
Garrett Moore <gmoore10@fullsail.edu>
2013-09-12 21:02:57 -0400

    Commit to gh-pages

---
+++ Week 2/js/json.js
@@ -0,0 +1,203 @@
+﻿{ 
+    "todos": [ {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Meet with Andy re: Data Project",
+        "dtDue": "2/18/2013",
+        "assignedTo": "James Makowski",
+        "priority": "27",
+        "sendEmail": "Yes",
+        "content": "Talk to Andy about incorporating ethnicity information into data tracking projects."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Add Search Feature to Data Project",
+        "dtDue": "2/18/2013",
+        "assignedTo": "Garrett Moore",
+        "priority": "33",
+        "sendEmail": "No",
+        "content": "Implement Search feature to find guest by first name, last name and email address."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Finalize Budget",
+        "dtDue": "2/18/2013",
+        "assignedTo": "James Makowski",
+        "priority": "37",
+        "sendEmail": "Yes",
+        "content": "Line-by-line review of annual budget. Submit to manager when complete"
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Add Guest System to Tour Database",
+        "dtDue": "2/19/2013",
+        "assignedTo": "Garrett Moore",
+        "priority": "44",
+        "sendEmail": "Yes",
+        "content": "Implement guest system for Tour Database."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Discuss Bussing Staff Requirements",
+        "dtDue": "2/19/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "18",
+        "sendEmail": "Yes",
+        "content": "Discuss bussing quantity and drivers needed with Jim."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Update Events Spreadsheet",
+        "dtDue": "2/19/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "2",
+        "sendEmail": "Yes",
+        "content": "Update spreadsheet with newly-generated events."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Contact Bussing Vendor for Event",
+        "dtDue": "2/18/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "96",
+        "sendEmail": "Yes",
+        "content": "Talk to Bussing Vendor to get invoice for previous event."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Contact Catering Vendor for Event",
+        "dtDue": "2/18/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "96",
+        "sendEmail": "Yes",
+        "content": "Get deposit invoice from catering company."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Verify Radio Count from Vendor",
+        "dtDue": "2/20/2013",
+        "assignedTo": "James Makowski",
+        "priority": "88",
+        "sendEmail": "Yes",
+        "content": "Verify 42 radios from Central Communcations."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Hold Lunch Meeting",
+        "dtDue": "2/21/2013",
+        "assignedTo": "James Makowski",
+        "priority": "99",
+        "sendEmail": "Yes",
+        "content": "Hold Lunch Meeting with Security, IMT, Runners, I&D, etc."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Prepare Table Top Signage",
+        "dtDue": "2/22/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "88",
+        "sendEmail": "Yes",
+        "content": "Verify that all signs were returned and print/prepare new ones as needed."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Prepare Paint Stick Signage",
+        "dtDue": "2/22/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "88",
+        "sendEmail": "Yes",
+        "content": "Verify all signs were returned and print/prepare new ones as needed."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Make Office Depot Trip",
+        "dtDue": "2/22/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "41",
+        "sendEmail": "Yes",
+        "content": "Buy: Lamination Sheets, Plastic Drawers, File Folders, Ink, and post-its."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Order Food from Crisper's",
+        "dtDue": "2/23/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "57",
+        "sendEmail": "Yes",
+        "content": "7 Sandwich platters minimu, assorted varieties. Make sure one is vegetarian."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Call Students to Schedule Work",
+        "dtDue": "2/22/2013",
+        "assignedTo": "James Makowski",
+        "priority": "91",
+        "sendEmail": "Yes",
+        "content": "43 Students required to work the event."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Prepare Student Emails",
+        "dtDue": "2/22/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "81",
+        "sendEmail": "Yes",
+        "content": "Prepare student work emails and setup/strike emails."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Send Student Emails",
+        "dtDue": "2/22/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "81",
+        "sendEmail": "Yes",
+        "content": "Send emails at 7PM on Friday."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Email Executive Committee",
+        "dtDue": "2/23/2013",
+        "assignedTo": "James Makowski",
+        "priority": "94",
+        "sendEmail": "Yes",
+        "content": "Email weather and numbers to Executive Committee."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Deliver Rain Ponchos",
+        "dtDue": "2/24/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "2",
+        "sendEmail": "Yes",
+        "content": "Drop 11 Bins of Rain Ponchos."
+    },
+    {
+        "firstName": "Garrett",
+        "lastName": "Moore",
+        "toDoName": "Print Master/Bussing/Orientation Schedules",
+        "dtDue": "2/23/2013",
+        "assignedTo": "Sarah Slegona",
+        "priority": "75",
+        "sendEmail": "Yes",
+        "content": "Make 5 copies of all schedules. Reserve one copy for Sarah Slegona."
+    }
+]
+}
---
+++ Week 2/js/main.js
@@ -0,0 +1,242 @@
+﻿//ASD 1309
+//Author: Garrett Moore
+//Refactored from VFW
+
+var toDoLibrary = {
+    pageInits: {
+        homeInit: function () {
+            //Load XML data for Drop Down List in form.
+            var toDoAssignees = $.ajax({
+                type: "GET",
+                url: "js/teamMembers.xml",
+                dataType: "xml",
+                success: function (data, status) {
+                    $(data).find('teamMember').each(function () {
+                        var $teamMember = $(this);
+                        var name = $teamMember.find('name').text();
+                        $("#ddlAssignedTo").append('<option value="' + name + '">' + name + '</option>');
+                    });
+                }
+            });
+            $("#addItemLink").on("click", function (event) {
+                toDoLibrary.operations.addToDo();
+            });
+        },
+        itemsInit: function () {
+            toDoLibrary.operations.getToDos();
+
+            var divItems = $("div#items div[data-role='content'] ul");
+
+            divItems.empty();
+
+            for (var i = 0, j = localStorage.length; i < j; i++) {
+
+                var key = localStorage.key(i);
+                var value = localStorage.getItem(key);
+                var todo = JSON.parse(value);
+
+                divItems.append('<li>' +
+                    '<a href="#addItem" data-key="' + key + '">' + todo.toDoName +
+                    '<br />Due: ' + todo.dtDue + '</a><button class=\'delToDo\' data-key=' + key + '>Delete</button>' +
+                    '</li>');
+
+                divItems.listview('refresh')
+
+            }
+
+            $("a[data-key][href='#addItem']").on("click", function (event) {
+                var toDoId = $(event.target).data('key');
+                toDoLibrary.operations.editToDo(toDoId);
+            });
+
+            $(".delToDo").on("click", function (event) {
+                var toDoId = $(event.target).data('key');
+                toDoLibrary.operations.deleteToDo(toDoId);
+            });
+        },
+        membersInit: function () {
+            var divMembers = $("div#members div[data-role='content'] ul");
+
+            var toDoTeamMembers = $.ajax({
+                type: "GET",
+                url: "js/teamMembers.xml",
+                dataType: "xml",
+                success: function (data, status) {
+                    $(data).find('teamMember').each(function () {
+                        var $teamMember = $(this);
+                        var name = $teamMember.find('name').text();
+                        divMembers.append('<li>' + name + '</li>');
+                    });
+                    divMembers.listview('refresh')
+                }
+            });
+        },
+        addItemInit: function () {
+            var toDoAssigneeList = {};
+
+            $("#ddlAssignedTo").selectmenu('refresh');
+
+            var myForm = $('#addToDoForm');
+            myForm.validate({
+                invalidHandler: function (form, validator) {
+                },
+                submitHandler: function () {
+                    var data = myForm.serializeArray();
+                    var key = $("#key").val();
+                    toDoLibrary.operations.storeData(data, key);
+                }
+            });
+        },
+        settingsInit: function () {
+            $(".clearLocal").on("click", function (event) {
+                toDoLibrary.operations.clearLocalStorage();
+            });
+        }
+    },
+    operations: {
+        editToDo: function (key) {
+            //Change to the #addItem page
+            $.mobile.changePage($('#addItem'));
+
+            //get the toDo from the passed key
+            var todo = localStorage.getItem(key);
+            var todoItem = JSON.parse(todo);
+
+            //Populate the form with the retrieved values
+            $("#txtFirstName").val(todoItem.firstName);
+            $("#txtLastName").val(todoItem.lastName);
+            $("#txtToDoName").val(todoItem.toDoName);
+            $("#txtDueDate").val(todoItem.dtDue);
+            $("#ddlAssignedTo").val(todoItem.assignedTo);
+            $("#ddlAssignedTo").selectmenu('refresh');
+            $("#rngPriority").val(todoItem.priority);
+            $("#sldEmailTaskReceiver").val(todoItem.sendEmail);
+            $("#txtContent").val(todoItem.content);
+
+            //Pass Key to hidden input
+            $("#key").val(key);
+
+            var link = $("#addItem input[type='submit']");
+
+            //Edit the submit button's value to say "Edit" instead of "Add"
+            $("#addItem input[type='submit']").attr("value", "Edit To-Do");
+            link.button('refresh');
+        },
+        addToDo: function () {
+            //Change to the #addItem page
+            $.mobile.changePage($('#addItem'));
+
+            var link = $("#addItem input[type='submit']");
+
+            //Edit the submit button's value to say "Edit" instead of "Add"
+            $("#addItem input[type='submit']").attr("value", "Add To-Do");
+            link.button('refresh');
+        },
+        storeData: function (data, key) {
+            if (!key) {
+                var id = Math.floor(Math.random() * 100000001);
+            } else {
+                id = key;
+            }
+            var item = {};
+            item.firstName = [data[0].value];
+            item.lastName = [data[1].value];
+            item.toDoName = [data[2].value];
+            item.dtDue = [data[4].value];
+            item.assignedTo = [data[3].value];
+            item.priority = [data[5].value];
+            item.sendEmail = [data[7].value];
+            item.content = [data[6].value];
+
+            //Save data into Local Storage: Use Stringify to convert our object to a string.
+            localStorage.setItem(id, JSON.stringify(item));
+            //alert("To-Do Saved!");
+
+            //Reset the Form
+            var myForm = $('#addToDoForm');
+            myForm[0].reset();
+            //Go Home
+            $.mobile.changePage($("#home"));
+        },
+        autoPopulateData: function () {
+            //Populate to-do records from JSON file it localStorage is empty.
+            var divItems = $("div#items div[data-role='content'] ul");
+            var todoItems = $.ajax({
+                type: "GET",
+                url: "js/json.js",
+                dataType: "json",
+                error: function (status) {
+                    console.log(status);
+                },
+                success: function (data) {
+                    console.log(data);
+                    for (var i = 0, j = data.todos.length; i < j; i++) {
+                        var todo = data.todos[i];
+
+                        var id = Math.floor(Math.random() * 100000001);
+                        localStorage.setItem(id, JSON.stringify(todo));
+
+                    }
+                    divItems.listview('refresh');
+                }
+            });
+            //for (var n in json) {
+            //    var id = Math.floor(Math.random() * 100000001);
+            //    localStorage.setItem(id, JSON.stringify(json[n]));
+            //}
+        },
+        getToDos: function () {
+            if (localStorage.length === 0) {
+                toDoLibrary.operations.autoPopulateData();
+            }
+        },
+        deleteToDo: function (key) {
+            var confirmPopup = confirm("Are you sure you want to delete this to-do?");
+            if (confirmPopup) {
+                localStorage.removeItem(key);
+                window.location.reload();
+            } else {
+                alert("No changes were made");
+            }
+        },
+        clearLocalStorage: function () {
+            if (localStorage.length === 0) {
+                alert("There is no data to clear.");
+            } else {
+                localStorage.clear();
+                alert("All to-do's are deleted!");
+                window.location.reload();
+                return false;
+            }
+        }
+    }
+}
+
+$('#home').on('pageinit', function () {
+    toDoLibrary.pageInits.homeInit();
+});
+
+$('#items').on('pageinit', function () {
+    toDoLibrary.pageInits.itemsInit();
+});
+
+$('#items').on('pageshow', function () {
+    toDoLibrary.pageInits.itemsInit();
+});
+
+$('#members').on('pageinit', function () {
+    toDoLibrary.pageInits.membersInit();
+});
+
+$('#addItem').on('pageinit', function () {
+    toDoLibrary.pageInits.addItemInit();
+});
+
+$('#addItem').on('pagehide', function () {
+    var myForm = $('#addToDoForm');
+    myForm[0].reset();
+});
+
+$('#settings').on('pageinit', function () {
+    toDoLibrary.pageInits.settingsInit();
+});
